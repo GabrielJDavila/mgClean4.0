1f537b1ea467dd08287cb39899a4adfdec3c6206
GabrielJDavila <107373187+GabrielJDavila@users.noreply.github.com>
2023-03-17 22:53:45 -0500

    updated CSS and added Carousel

--- mgclean-4.0/src/Main.jsx
+++ mgclean-4.0/src/Main.jsx
@@ -1,13 +1,39 @@
+import React, { useState } from 'react'
+
 export default function Main() {
+    const images = ['1', '2', '3', '4']
+    const [currentIndex, setCurrentIndex] = useState(0)
+
+    const nextImg = () => {
+        if(currentIndex === images.length - 1) {
+            return setCurrentIndex(0)
+        }
+        return setCurrentIndex(currentIndex + 1)
+    }
+    const prevImg = () => {
+        if(currentIndex === images.length - 4) {
+            return setCurrentIndex(3)
+        }
+        return setCurrentIndex(currentIndex - 1)
+    }
+
     return (
         <main>
-            <section className="section">
+            <section className="section hero">
+                <h2 className="section-title">Hero</h2>
+                <div>
+                    <h2>{images[currentIndex]}</h2>
+                    <button onClick={nextImg}>Next</button>
+                    <button onClick={prevImg}>Prev</button>
+                </div>
+            </section>
+            <section className="section services">
                 <h2 className="section-title">Services</h2>
             </section>
-            <section className="section">
+            <section className="section book">
                 <h2 className="section-title">Book Now</h2>
             </section>
-            <section className="section">
+            <section className="section about">
                 <h2 className="section-title">About</h2>
             </section>
         </main>
--- mgclean-4.0/src/Nav.jsx
+++ mgclean-4.0/src/Nav.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
+import image1 from './assets/mgcleanpros-transparent.png'
 
 export default function Navbar() {
     const [menu, setMenu] = useState(false)
@@ -12,7 +13,7 @@ export default function Navbar() {
         <div className='nav-div'>
             <header>
                 <div className="title-div">
-                    <h1>M&G Clean Pros LLC</h1>
+                    <img src={image1} className='logo'></img>
                 </div>
                 <div className="hamburger" onClick={toggleMenu}>
                     {menu ? <i className="fa-solid fa-xmark"></i> : <i className="fa-solid fa-bars"></i> }
